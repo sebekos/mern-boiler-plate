c0ef53caf8612dec539c285bb0e6c01b882b27ad
Sebastian Kosela <sebkpl@gmail.com>
2020-03-17 14:06:01 -0500

    auth and register

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
-const dotenv = require("dotenv");
 
 // Init express
 const app = express();
@@ -12,6 +11,10 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 
-// Setup porta
+// Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/user", require("./routes/api/user"));
+
+// Setup port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
